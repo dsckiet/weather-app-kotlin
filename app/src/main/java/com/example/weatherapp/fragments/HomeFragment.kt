2ad3a8ad4209d295e5b0f64b03ea7cd74ec6e2c6
ananyapunia28 <ananyapunia12@gmail.com>
2021-07-19 13:53:15 +0530

    Navigation Drawer

--- app/src/main/java/com/example/weatherapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/HomeFragment.kt
@@ -10,12 +10,6 @@ import com.example.weatherapp.R
 class HomeFragment : Fragment() {
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        }
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
